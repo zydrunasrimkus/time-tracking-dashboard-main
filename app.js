8d6b8a66b992fbc59290aff68ac8e02b0af5ed39
zydrunasrimkus <zydrunas.rimkus95@gmail.com>
2021-11-19 18:27:41 +0200

    package json fixed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static(__dirname+'/public'));
 
+app.use(express.urlencoded({extended:true}));
+
 app.get('/', (req, res) => {
     res.render("/index.html");
 })
